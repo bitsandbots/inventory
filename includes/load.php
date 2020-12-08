b0f217a2a8ae59214fefdcc8f953a8ee701fe31c
bitsandbots <bitsandbots@gmail.com>
2020-12-08 10:54:34 -0800

    delete stock before products

--- includes/load.php
+++ includes/load.php
@@ -1,10 +1,8 @@
-
 <?php
 // -----------------------------------------------------------------------
 // DEFINE SEPERATOR ALIASES
 // -----------------------------------------------------------------------
 define("URL_SEPARATOR", '/');
-
 define("DS", DIRECTORY_SEPARATOR);
 
 // -----------------------------------------------------------------------
@@ -12,7 +10,6 @@ define("DS", DIRECTORY_SEPARATOR);
 // -----------------------------------------------------------------------
 defined('SITE_ROOT')? null: define('SITE_ROOT', realpath(dirname(__FILE__)));
 define("LIB_PATH_INC", SITE_ROOT.DS);
-
 
 require_once(LIB_PATH_INC.'config.php');
 require_once(LIB_PATH_INC.'functions.php');
@@ -22,9 +19,19 @@ require_once(LIB_PATH_INC.'database.php');
 require_once(LIB_PATH_INC.'sql.php');
 require_once(LIB_PATH_INC.'formatcurrency.php');
 
+/*--------------------------------------------------------------*/
+/* Change format of currency used throughout the system
+/*--------------------------------------------------------------*/
 $CURRENCY_CODE = 'USD';
 //$CURRENCY_CODE = 'EUR';
+
+
+/*--------------------------------------------------------------*/
+/* Log user actions
+/*--------------------------------------------------------------*/
 $user_id = 0;
+$remote_ip = 0;
+$action =  '';
 if (isset( $_SESSION['user_id'] ))
 {
 $user_id = $_SESSION['user_id'];
@@ -37,12 +44,21 @@ if ( isset($_SERVER['HTTP_X_FORWARDED_FOR']) )
 		$remote_ip_for = explode( ",", $remote_ip );
 		$remote_ip = $remote_ip_for[0];
 	}
-} else
+} else {
+
+if (isset( $_SERVER['REMOTE_ADDR'] ))
 {
 	$remote_ip = $_SERVER['REMOTE_ADDR'];
 }
+
+}
+
+if (isset( $_SERVER['REQUEST_URI'] ))
+{
 $action = $_SERVER['REQUEST_URI'];
 $action = preg_replace('/^.+[\\\\\\/]/', '', $action);
 //$action = preg_replace('/^\/inventory/', '', $action);
+}
+
 logAction( $user_id, $remote_ip, $action );
 ?>
--- products/delete_product.php
+++ products/delete_product.php
@@ -4,8 +4,6 @@
  *
  * @package default
  */
-
-
 require_once '../includes/load.php';
 // Checkin What level user has permission to view this page
 page_require_level(2);
@@ -13,17 +11,25 @@ page_require_level(2);
 <?php
 $product = find_by_id('products', (int)$_GET['id']);
 if (!$product) {
-	$session->msg("d", "Missing Product id.");
+	$session->msg("d", "Missing Product ID.");
 	redirect('../products/products.php');
 }
-?>
-<?php
+
+$all_stock = find_all('stock');
+foreach ( $all_stock as $stock ) {
+	if ( $stock['product_id'] == $product['id'] )
+	{
+	$session->msg("d", "Please delete entries OR add negative quantity stock.");
+	redirect('../products/stock.php');
+	}
+}
+
 $delete_id = delete_by_id('products', (int)$product['id']);
 if ($delete_id) {
-	$session->msg("s", "Products deleted.");
+	$session->msg("s", "Product deleted.");
 	redirect('../products/products.php');
 } else {
-	$session->msg("d", "Products deletion failed.");
+	$session->msg("d", "Failed to delete Product.");
 	redirect('../products/products.php');
 }
 ?>
