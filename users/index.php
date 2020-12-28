4044740d5d1acfaa0b63b581eed80f315d06f837
Cali <41587193+cleverguns@users.noreply.github.com>
2020-12-27 19:14:38 -0800

    adding animation and show password on landing page
     I add animation effect and I hope you like it.

--- users/index.php
+++ users/index.php
@@ -1,25 +1,66 @@
+<!--includes -->
+
 <?php
 ob_start();
 require_once '../includes/load.php';
-
+//importing csrf handler
+use csrfhandler\csrf as csrf;
 if ($session->isUserLoggedIn()) { redirect('../users/home.php', false);}
 ?>
 <?php include_once '../layouts/header.php'; ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!--animation link -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.compat.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.compat.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.css">
+</head>
+<body>
+
+<div class="animate__animated animate__backInDown" >
 <div class="login-page">
     <div class="text-center">
-       <h1>Welcome</h1>
+       <h1 class="animate__animated animate__flip animate__delay-2s" > Welcome</h1>
        <p>Sign in to start your session</p>
      </div>
      <?php echo display_msg($msg); ?>
+
       <form method="post" action="../users/auth.php" class="clearfix">
+      <input type="hidden" name="_token" value="<?php echo csrf::token()?>">
         <div class="form-group">
+        
               <label for="username" class="control-label">Username</label>
-              <input type="name" class="form-control" name="username" placeholder="Username">
+              <input type="name" class="form-control animate__animated animate__lightSpeedInLeft animate__delay-3s" name="username" placeholder="Username">
         </div>
-        <div class="form-group">
+        <div class="form-group">       
             <label for="Password" class="control-label">Password</label>
-            <input type="password" name= "password" class="form-control" placeholder="Password">
+            <input type="password"  id="UserInput" name= "password" class="form-control animate__animated animate__lightSpeedInLeft animate__delay-3s " placeholder="Password">
+
+            <br>
+            <input type="checkbox" onclick="myFunction()"> Show Password
+            <!-- show password -->
+                                <script>
+                    function myFunction() {
+                      var x = document.getElementById("UserInput");
+                      if (x.type === "password") {
+                        x.type = "text";
+                      } else {
+                        x.type = "password";
+                      }
+                    }
+                    </script>
+
         </div>
+
+        
+
         <div class="form-group">
                 <button type="submit" class="btn btn-info  pull-right">Login</button>
         </div>
@@ -28,4 +69,15 @@ if ($session->isUserLoggedIn()) { redirect('../users/home.php', false);}
        <p></p>
      </div>
 </div>
+
+</div>
+
+
+
+
 <?php include_once '../layouts/footer.php'; ?>
+</body>
+</html>
+
+
+
